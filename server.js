737fa7d9c5ea18ce7a7dcd3f15903138cebfe10a
Charan sai <charantherockin1998@gmail.com>
2025-01-13 16:15:32 +0530

    added cors

--- server.js
+++ server.js
@@ -5,15 +5,18 @@ import {router} from "./routes/contactRoutes.js";
 import {userRouter} from "./routes/userRoutes.js"
 import { errorHandler } from './middlewares/errorHandler.js';
 import { connectDb } from './config/dbConnnection.js';
+import cors from "cors";
+
 
 dotenv.config();
 connectDb();
 
 const port= process.env.PORT ;
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use("/api/contacts",router);
 app.use("/api/users",userRouter);
 app.use(errorHandler);
